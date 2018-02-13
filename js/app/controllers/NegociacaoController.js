1467b2eb622c5ec7154a565cabddc90c5f33097f
Edi Carlos note <edi_csilva@hotmail.com>
2018-02-13 08:24:50 -0200

    usando proxy

--- js/app/controllers/NegociacaoController.js
+++ js/app/controllers/NegociacaoController.js
@@ -7,7 +7,10 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
-        this._listaNegociacoes = new ListaNegociacoes();
+        this._listaNegociacoes = new ListaNegociacoes((model) => {
+            // console.log(this);
+            this._NegociacoesView.update(model);
+        });
 
         this._NegociacoesView = new NegociacoesView($('#negociacoesView'));
         this._NegociacoesView.update(this._listaNegociacoes);
@@ -21,7 +24,7 @@ class NegociacaoController {
         event.preventDefault();
 
         this._listaNegociacoes.adiciona(this._criaNegociacao());
-        this._NegociacoesView.update(this._listaNegociacoes);
+        // this._NegociacoesView.update(this._listaNegociacoes);
 
         this._mensagem.texto = 'Negociacao adicionada com sucesso';
         this._mensagemView.update(this._mensagem);
@@ -44,5 +47,14 @@ class NegociacaoController {
         this._inputValor.value = 0.0;
 
         this._inputData.focus();
+    }
+
+    apaga() {
+
+        this._listaNegociacoes.esvazia();
+        // this._NegociacoesView.update(this._listaNegociacoes);
+
+        this._mensagem.texto = 'Negociações apagadas com sucesso.';
+        this._mensagemView.update(this._mensagem);
     }
 }
